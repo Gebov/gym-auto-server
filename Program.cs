5230076ab377407559a91b924fa32dd269442247
gebov <martin.gebov@gmail.com>
2016-11-11 01:09:34 +0200

    form url encoded token login

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Text;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApplication1
 {
--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Http;
+using Gym.Auth.Model;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        private static readonly string secretKey = "mysupersecret_secretkey!123";
+
         public void Configure(IApplicationBuilder app)
         {
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+            var options = new TokenProviderOptions
+            {
+                Audience = "ExampleAudience",
+                Issuer = "ExampleIssuer",
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+            };
+ 
+            app.UseMiddleware<TokenProviderMiddleware>(Options.Create(options));
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
